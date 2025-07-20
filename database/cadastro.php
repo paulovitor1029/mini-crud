3f1dd6a9ee32eebbc640ba300c973fba22ac0fd4
Paulo Vitor <pv.annunciatollata@gmail.com>
2025-07-20 17:01:30 -0300

    fixes the issue of sending to the database over and over again

--- database/cadastro.php
+++ database/cadastro.php
@@ -1,11 +1,28 @@
 <?php 
-    
-    try {
-        $pdo = new PDO('sqlite:db/crud.sqlite');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Erro ao conectar ao banco de dados:" . $e->getMessage();
-        exit;
-    }
+   
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $nome = trim($_POST['nome']);
+        $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+        $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+        $cargo = $_POST['cargo'];
 
+        if ($nome && $email) {
+            try {
+                $pdo = new PDO('sqlite:' .  __DIR__ . '/crud.sqlite');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $query = "INSERT INTO usuarios (nome, email, senha, cargo) VALUES ('$nome', '$email', '$senha', '$cargo')";
+                $pdo->exec($query);
+
+                header('Location: ../dashboard.php');
+
+            } catch (PDOException $e) {
+                echo "Erro ao conectar ao banco de dados:" . $e->getMessage();
+                exit;
+            }
+        } else {
+            echo "Dados inválidos";
+        }
+    }
+    $pdo = null;
 ?>
